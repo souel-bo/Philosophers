c1a2a9a15b05f4dde78fdfaa523f5b77a1a3cc2c
Soufiane El Bourakkadi <souel-bo@e2r8p4.1337.ma>
2025-03-07 05:00:39 +0000

    repeating the code from scratch

--- includes/philo.h
+++ includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sfyn <sfyn@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 10:11:55 by souel-bo          #+#    #+#             */
-/*   Updated: 2025/03/02 20:11:40 by sfyn             ###   ########.fr       */
+/*   Updated: 2025/03/07 04:45:36 by souel-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,36 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
+
 typedef struct s_philo
 {
-    //number_of_philosophers//////time_to_die/////time_to_eat/////////time_to_sleep/////[number_of_times_each_philosopher_must_eat]
-    unsigned long philos;
-    unsigned long death;
-    unsigned long eat;
-    unsigned long sleep;
-    unsigned long meals;
-    pthread_t	*threads;
-    pthread_t   monitor;
-	pthread_mutex_t	protect;
-    pthread_mutex_t	*mutexes;
-    pthread_mutex_t	*left_fork;
-    pthread_mutex_t	*right_fork;
-    int id;
-    int arguments;
-    long long current_time;
-    long long simulation_time;
-    long long last_meal;
-    int *dead;
+	pthread_t		thread;
+	int				id;
+	int				meals_eaten;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*left_fork;
 } t_philo;
 
+
+typedef struct s_program
+{
+    unsigned int num_philo;
+    unsigned int time_to_die;
+    unsigned int num_meals;
+    unsigned int time_to_sleep;
+    unsigned int time_to_eat;
+    t_philo *philos;
+} t_program ;
+
 unsigned long	ft_atoi(const char *str);
-void 			initialize_thread(t_philo *threads, int arguments);
 long long get_time(void);
 
 #endif
--- main.c
+++ main.c
@@ -6,69 +6,19 @@
 /*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 10:11:37 by souel-bo          #+#    #+#             */
-/*   Updated: 2025/02/27 00:42:05 by souel-bo         ###   ########.fr       */
+/*   Updated: 2025/03/07 05:00:00 by souel-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/philo.h"
 
-t_philo *check_count(char **arguments, int argc)
-{
-	t_philo	*content;
-
-	content = malloc(sizeof(t_philo));
-	if (!content)
-		return (NULL);
-	content->philos = ft_atoi(arguments[1]);
-	if ( content->philos == 0 || content->philos > 2147483647)
-	{
-		free(content);
-		return NULL;
-	}
-	content->death = ft_atoi(arguments[2]);
-	if (content->death == 9999999999999 || content->death > 2147483647)
-	{
-		free(content);
-		return NULL;
-	}
-	content->eat = ft_atoi(arguments[3]);
-	if (content->eat == 9999999999999 || content->eat > 2147483647)
-	{
-		free(content);
-		return NULL;
-	}
-	content->sleep = ft_atoi(arguments[4]);
-	if (content->sleep == 9999999999999 || content->sleep > 2147483647)
-	{
-		free(content);
-		return NULL;
-	}
-	if (argc == 6)
-	{
-		content->meals = ft_atoi(arguments[5]);
-		if (content->meals == 9999999999999 || content->meals == 0 || content->meals > 2147483647)
-		{
-			free(content);
-			return NULL;
-		}
-	}
-	return content;
-}
-
 int	main(int argc, char *argv[])
 {
-	t_philo *threads;
-	if (argc < 4 || argc > 6)
-		return (write(2, "not enough arguments\n", 21), (1));
-	threads = check_count(argv, argc);
-	if (!threads)
-	{
-		printf("failed");
+	t_program program;
+	t_philo philos[200];
+	pthread_mutex_t forks[200];
+	if (argc != 5 && argc != 6)
 		return (1);
-	}
-	else
-		initialize_thread(threads, argc);
-	free(threads->threads);
-	free(threads->mutexes);
-	free(threads);
+	if (!parse_input(argv, &program))
+		return (1);
 }
